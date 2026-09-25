9175af1b6cef8f4974ab01dded0cca39588b288f
DeepayanDas <deepayan656@gmail.com>
2026-09-25 17:58:12 +0530

    Fix Router import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import StudentDashboard from './pages/StudentDashboard';
 import InstructorDashboard from './pages/InstructorDashboard';
@@ -8,14 +8,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public Login Route */}
+        {/* Public Login & Sign Up Route */}
         <Route path="/" element={<Login />} />
 
-        {/* Protected Dashboard Routes */}
+        {/* Protected Portal Workspaces */}
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/instructor" element={<InstructorDashboard />} />
 
-        {/* Fallback Catchall redirects to Login */}
+        {/* Catchall Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
